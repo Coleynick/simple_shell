383c36fefd8ddcf3b328f3d741a694a4f186a793
Coleynick <ezechidinma14@gmail.com>
2023-06-15 06:38:12 -0700

    Pass the Betty checks

---
+++ 0_betty.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - Starting point
+ * Return: Successful if 0
+ */
+
+int main(void)
+{
+	char c[5] = {'C', 'h', 'i', 'n', 'a'};
+
+	for (x = 0; x < 5; x++)
+	{
+		write(1, &c[x], sizeof(char));
+	}
+	return (0);
+}
