f51ea1df62e19af6b1448a648af45b3b00dbd588
Coleynick <ezechidinma14@gmail.com>
2023-06-18 15:02:19 -0700

    Implement the exit built-in, that exits the shell

--- simple_shell04.c
+++ simple_shell04.c
@@ -5,31 +5,49 @@
 */
 int main(void)
 {
-char *av[20], *buffer = NULL;
-int num = 0, running = 1, input;
-
-while (running)
-{
-	printf("#cisfun$ ");
-	input = input_handle(&buffer, av);
-	if (input == -1 || input == 2)
-		running = 0;
-	if (input != -1)
+	char *av[20], *arg_s, *buffer = NULL;
+	pid_t pid;
+	int status, num = 0, running = 1, input;
+	while (running)
 	{
-		if (buffer[0] == '\0')
-			continue;
-		num = arguments(&buffer, av);
-		if (!num)
+		printf("#cisfun$ ");
+		input = input_handle(&buffer, av);
+		if (input == -1 || input == 2)
+			running = 0;
+		if (input != -1)
 		{
-			if (execve(av[0], av, environ) == -1)
-				perror(av[0]);
-		}
-		else
-		{
-			running = _fork(av);
+			if (buffer[0] == '\0')
+				continue;
+			if (strcmp(buffer, "exit") == 0)
+				break;
+			arg_s = strtok(buffer, " ");
+			for (num = 0; arg_s != NULL; num++)
+			{
+				av[num] = arg_s;
+				arg_s = strtok(NULL, " ");
+			}
+			av[num] = NULL;
+			pid = fork();
+			if (pid == 0)
+			{
+				if (execvp(av[0], av) == -1)
+				{
+					perror(av[0]);
+					exit(EXIT_FAILURE);
+				}
+			}
+			else if (pid == -1)
+			{
+				running = 0;
+			}
+			else
+			{
+				wait(&status);
+				if (!(WIFEXITED(status) && (WEXITSTATUS(status) == 0)))
+					running = 0;
+			}
 		}
 	}
-}
-free(buffer);
-return (running ? 0 : -1);
+	free(buffer);
+	return (running ? 0 : 1);
 }
