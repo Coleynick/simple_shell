e5eae15a69adb1df3a83ccc3c45dd79312d2125e
Coleynick <ezechidinma14@gmail.com>
2023-06-21 10:49:40 -0700

    Handle exit status

--- simple_shell08.c
+++ simple_shell08.c
@@ -1,32 +1,5 @@
 #include "main.h"
 /**
-* tokenArg - Tokenizes the buffer into arguments
-* @buffer: input buffer
-* @av: arguments
-* Return: Num
-*/
-int tokenArg(char *buffer, char **av)
-{
-	char *arg_s;
-	int num = 0;
-
-	if (buffer == NULL || *buffer == '\0')
-	{
-		av[num] = NULL;
-		return (num);
-	}
-	arg_s = strtok(buffer, " ");
-	while (arg_s != NULL)
-	{
-		av[num] = arg_s;
-		arg_s = strtok(NULL, " ");
-		num++;
-	}
-	av[num] = NULL;
-
-	return (num);
-}
-/**
 * main - Starting point
 * Return: 0 on success
 */
